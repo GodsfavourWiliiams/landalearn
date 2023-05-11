5ced8a0a9ee138691ea2abb8f0a853dd0e8f1962
Godsfavour Williams <godzfeivvur@gmail.com>
2023-05-11 17:05:07 +0100

    fixed navbar  style issues

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -28,7 +28,7 @@ const Sidebar: React.FC = () => {
   const router = useRouter();
 
   return (
-    <nav className="z-50 md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden bg-[#F6F9FD] flex flex-wrap items-center justify-between relative md:w-[270px] py-4 px-4 border border-[#E6E6E6]">
+    <nav className="z-50 md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden bg-[#F6F9FD] flex flex-wrap items-center justify-between relative md:w-[270px] py-4 px-2 md:px-5 border border-[#E6E6E6]">
       <div className="md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap items-center justify-between w-full mx-auto">
         {/* Toggler */}
         <div className="flex items-center">
